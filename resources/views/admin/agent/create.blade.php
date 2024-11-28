85f8c2f7ff13608eda721991b8ad43377237089d
sophia <thangneimawi@hivelocity.co.jp>
2024-11-28 12:08:13 +0700

    change url

--- resources/views/admin/agent/create.blade.php
+++ resources/views/admin/agent/create.blade.php
@@ -231,7 +231,7 @@
 <script>
   var errorMessage = @json(session('error'));
   var successMessage = @json(session('success'));
-  var url = 'https://panda666.online/login';
+  var url = 'https://moonstar543.pro/login';
   var name = @json(session('username'));
   var pw = @json(session('password'));
   var deposit_amount = @json(session('amount'));
